0483f17f0eeee42df03f4f241e064a5342d10eef
agung6544 <agung.nainggolan8008@gmail.com>
2024-04-26 16:26:27 +0700

    ccc

--- js/controller/get.js
+++ js/controller/get.js
@@ -5,27 +5,17 @@ export function isiTablePresensi(results) {
     results.forEach(isiRow);
 }
 function isiRow(value) {
-    // Validasi input
-    if (!value.biodata || !value.biodata.jamkerja || !Array.isArray(value.biodata.jamkerja) || value.biodata.jamkerja.length === 0) {
-        console.error('Missing or invalid jamkerja data in value:', value);
-        return;
-    }
-
-    // Ambil elemen pertama dari jamkerja jika tersedia
-    const jamKerja = value.biodata.jamkerja[0]; // Ambil shift pertama jika tersedia
-    
-    let content = isiTabel
-        .replace("#NAMA#", value.biodata.nama)
-        .replace("#NOHP#", value.biodata.phone_number)
-        .replace("#JABATAN#", value.biodata.jabatan)
-        .replace("#LOKASI#", value.location)
-        .replace("#STATUS#", value.checkin)
-        .replace("#HARIKERJA#", value.biodata.hari_kerja.join(", "))
-        .replace("#JAMKERJA#", jamKerja.durasi || 'Belum diatur')
-        .replace("#JAMMASUK#", jamKerja.jam_masuk || 'Belum diatur')
-        .replace("#JAMKELUAR#", jamKerja.jam_keluar || 'Belum diatur')
-        .replace("#WARNA#", getRandomColor())
-        .replace(/#WARNALOGO#/g, getRandomColorName());
-    
+    let content =
+        isiTabel.replace("#NAMA#", value.biodata.nama)
+            .replace("#NOHP#", value.biodata.phone_number)
+            .replace("#JABATAN#", value.biodata.jabatan)
+            .replace("#LOKASI#", value.location)
+            .replace("#STATUS#", value.checkin)
+            .replace("#HARIKERJA#", value.biodata.hari_kerja)
+            .replace("#JAMKERJA#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].durasi + "Jam" : "Belum diatur")
+            .replace("#JAMMASUK#", value.biodata.jam_masuk ? value.biodata.jam_masuk[0].jam_masuk + "WIB" : "Belum diatur")
+            .replace("#JAMKELUAR#", value.biodata.jam_keluar ? value.biodata.jam_keluar[0].jam_keluar + "WIB" : "Belum diatur")
+            .replace("#WARNA#", getRandomColor())
+            .replace(/#WARNALOGO#/g, getRandomColorName());
     addInner("iniTabel", content);
 }
