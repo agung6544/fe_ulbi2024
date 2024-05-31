13056af23d0ce5006095c2db3c63b6d5880cca2e
agung6544 <agung.nainggolan8008@gmail.com>
2024-05-31 16:19:53 +0700

    week10

---
+++ js/config/url_post.js
@@ -0,0 +1,7 @@
+export let urlPOST = "https://ws-agung-2024-b77249100221.herokuapp.com/insert"
+
+export function AmbilResponse(result) {
+    console.log(result); //menampilkan response API pada console
+    alert(result.message); //menampilkan response API pada alert
+    window.location.reload(); //reload halaman setelah klik ok pada alert
+}
